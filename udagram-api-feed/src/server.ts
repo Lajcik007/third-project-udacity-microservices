4312cedd9258d7ed27ac57fb8ef3fced30d08b2b
mpowalka <michal.powalka@mobilereality.pl>
2020-11-13 22:19:11 +0100

    improvement

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -35,6 +35,10 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
     res.send( '/api/v0/' );
   } );
 
+  app.get('/health', async (req, res): Promise<void> => {
+    res.status(200).send('ok');
+  });
+
 
   // Start the Server
   app.listen( port, () => {
